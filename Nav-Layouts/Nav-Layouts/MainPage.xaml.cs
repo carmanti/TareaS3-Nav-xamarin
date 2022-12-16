11d0a0d1799c798bf4ff2db4b48799c5c7d5b6fd
carmanti <mantilla.carlosjoselo@gmail.com>
2022-12-16 15:52:02 -0500

    Commit homework

---
+++ Nav-Layouts/Nav-Layouts/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Nav_Layouts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void btnAbrirVentanaDos_Clicked(object sender, EventArgs e)
+        {
+            String user = txtUser.Text;
+            String pass = txtPass.Text;
+
+            await Navigation.PushAsync(new Ventana_Dos(user, pass));
+        }
+    }
+}
---
+++ Nav-Layouts/Nav-Layouts/Ventana_Dos.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace Nav_Layouts
+{
+    internal class Ventana_Dos : Page
+    {
+        private string user;
+        private string pass;
+
+        public Ventana_Dos(string user, string pass)
+        {
+            this.user = user;
+            this.pass = pass;
+        }
+    }
+}
